17afea58c6c129d31701bef984c2ec80cfb58224
kathirraja48 <kathirraja48@gmail.com>
2021-01-31 21:45:58 +0530

    jobs api

--- django_twitter/twitter_app/urls.py
+++ django_twitter/twitter_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('get_profile', views.get_profile, name="get_profile"),
     path('get_timeline', views.get_timeline, name="get_timeline"),
     path('get_timeline_from_db', views.get_timeline_from_db, name="get_timeline_from_db"),
+    path('update_timeline_job', views.update_timeline_job, name="update_timeline_job"),
 ]
